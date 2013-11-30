fe9e802dec020207e67e91f78c7a8e8406050162
Michael Dawson <nippysaurus@me.com>
2013-11-30 16:32:06 +1000

    hacking

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// 
+// X-FACTOR
+//
+// version: 1.0
+// authors: @ssanj & @nippysaurus
+//
+
 var five = require("johnny-five"),
     board, lcd;
 
@@ -18,9 +25,11 @@ board.on("ready", function() {
     freq: 50  
   });
 
+  var running = true;
+  var print_game_over = true;
   var score = 0;
 
-  var man_col = 1,
+  var man_col = 2,
       man_row = 1,
       man_row_zero_for = 0,
       man_cannot_jump_for = 0;
@@ -28,20 +37,26 @@ board.on("ready", function() {
 
   var frame = 1;
 
-
   var a = lcd.useChar("runninga");
   var b = lcd.useChar("runningb");
 
-  var obstacle_col = null;
+  var obstacle_cols = [];
 
   var collision_check = function() {
-    if (man_col != obstacle_col) { return false; }
-    if (man_row == 1) { return true; }
-    else { score++; return false; }
+    for(var i=0;i<obstacle_cols.length;i++) {
+      var obstacle_col = obstacle_cols[i];
+      if (man_col != obstacle_col) { continue; }
+      if (man_row == 1) {
+        return true;
+      } else {
+        score++;
+        return false;
+      }
+    }
   };
 
   var generate_obstacle = function() {
-    obstacle_col = lcd.cols;
+    obstacle_cols.push(lcd.cols);
   };
 
   generate_obstacle();
@@ -50,7 +65,7 @@ board.on("ready", function() {
     if ((this.value < 3) && (man_row_zero_for == 0) && (man_cannot_jump_for == 0)) {
       man_row = 0;
       man_row_zero_for = 4;
-      man_cannot_jump_for = 1;
+      man_cannot_jump_for = 2;
     }
   });
 
@@ -58,13 +73,36 @@ board.on("ready", function() {
 
     board.loop(150, function() {
 
+      if (!running) {
+        if (print_game_over) {
+          // GAME OVER
+          lcd.clear();
+          lcd.cursor( 0, 3 ).print('GAME  OVER');
+          lcd.cursor( 1, 7 ).print(score);
+          print_game_over = false;
+        }
+
+        setTimeout((function() {
+          process.exit();
+        }), 200);
+
+        return;
+      }
+
       if (man_row_zero_for > 0) {
         man_row_zero_for--;
       } else {
         man_row = 1;
       }
 
-      if (obstacle_col == 0) {
+      for(var i=0;i<obstacle_cols.length;i++) {
+        if (obstacle_cols[i] != 0) {
+          obstacle_cols[i]--;
+        }
+      }
+
+      var random = Math.floor((Math.random()*10)+1);
+      if (random == 5) {
         generate_obstacle();
       }
 
@@ -78,23 +116,18 @@ board.on("ready", function() {
         ":running" + (++frame % 2 === 0 ? "a" : "b") + ":"
       );
 
-      lcd.cursor( obstacle_row, (obstacle_col-1) ).print('X');
+      for(var i=0;i<obstacle_cols.length;i++) {
+        if (obstacle_cols[i] != 0) {
+          lcd.cursor( obstacle_row, obstacle_cols[i] ).print('X');
+        }
+      }
+
       lcd.cursor( 0, 14 ).print(score);
 
-      if (obstacle_col) {
-        obstacle_col -= 1;
-        if (collision_check()) {
-          // GAME OVER
-          lcd.clear().cursor( 0, 0 ).print("GAME OVER");
-          //console.log(score);
-          process.exit();
-        }
+      if (collision_check()) {
+        running = false;
       }
     });
 
   });
 });
-
-
-// @device [16 x 2 LCD White on Blue](http://www.hacktronics.com/LCDs/16-x-2-LCD-White-on-Blue/flypage.tpl.html)
-// @device [20 x 4 LCD White on Blue](http://www.hacktronics.com/LCDs/20-x-4-LCD-White-on-Blue/flypage.tpl.html)
